4aae41c6976795610c93837e484408d67b37d536
pavalucas <lucasapava@gmail.com>
2026-02-08 23:07:44 -0300

    Update vercel.json

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,8 +4,4 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    // Output to a 'dist' directory at the root of the project
-    outDir: "../dist",
-  },
 });
